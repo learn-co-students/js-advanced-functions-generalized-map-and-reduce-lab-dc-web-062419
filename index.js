25f906f645eea9e209deb466e90b22452ff0ed26
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-12 23:55:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(arr, callback) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(callback(arr[i]));
+  }
+  return result;
+}
+
+function reduce(arr, callback, start) {
+  let accumulator = start ? start : arr[0];
+  let i = start ? 0 : 1;
+
+  for (; i < arr.length; i++) {
+    accumulator = callback(arr[i], accumulator);
+  }
+
+  return accumulator;
+}
